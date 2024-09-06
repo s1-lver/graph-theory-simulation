72f55f4fa86a96d74d802fe6efeac08dc29c6166
s1.lver <angelo1312fernandes@outlook.com>
2024-09-06 14:27:12 +0100

    before chaos

---
+++ script.js
@@ -0,0 +1,134 @@
+const coords = [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1],
+    [1, 2],
+]
+
+let points = []
+let lines = []
+
+let connections = {
+
+}
+
+class Point {
+    constructor(pos) {
+        this.pos = pos;
+
+        this.draw = function () {
+            game.ctx.beginPath();
+            game.ctx.arc(this.pos.x, this.pos.y, 5, 0, 2 * Math.PI);
+            game.ctx.fillStyle = "orange";
+            game.ctx.fill();
+        };
+    }
+}
+
+class Line {
+    constructor(p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+
+        this.draw = function () {
+            game.ctx.beginPath();
+            game.ctx.moveTo(this.p1.pos.x, this.p1.pos.y);
+            game.ctx.lineTo(this.p2.pos.x, this.p2.pos.y);
+            game.ctx.stroke();
+        }
+    }
+}
+
+
+let game = {
+    start: function() {
+        this.canvas = document.querySelector("canvas");
+        this.ctx = this.canvas.getContext("2d");
+
+    //     this.canvas.height = window.innerHeight;
+    //     this.canvas.width = window.innerWidth;
+        this.canvas.height = 600;
+        this.canvas.width = 600;
+
+        for (let i = 0; i < coords.length; i++) {
+            points.push(new Point(new Vector2D((coords[i][0]*100)+50, this.canvas.height-((coords[i][1]*100)+50))))
+        }
+
+        
+        for (let i = 0; i < coords.length; i++){
+            for (let j = 0; j < coords.length; j++){
+                if (i !== j) {
+                    if (Math.abs(coords[i][0] - coords[j][0]) <= 1 && Math.abs(coords[i][1] - coords[j][1]) <= 1 || Math.abs(coords[i][1] - coords[j][1]) <= 1 && Math.abs(coords[i][0] - coords[j][0]) <= 1) {
+                        lines.push(new Line(points[i], points[j]));
+                        
+                        if (i in connections) {
+                            connections[i] += 1
+                        } else {
+                            connections[i] = 1
+                        }
+
+                        if (j in connections) {
+                            connections[j] += 1
+                        } else {
+                            connections[j] = 1
+                        }
+                    }
+                }
+            }
+        }
+
+
+        let odd = 0;
+        let even = 0;
+
+        for (let key in connections) {
+            connections[key] /= 2;
+
+            if (connections[key] % 2 == 0) {
+                even += 1;
+            } else {
+                odd += 1;
+            }
+        }
+
+        if (odd > 2 || odd == 1) {
+            console.log("IMPOSSIBLE")
+        } else {
+            console.log("POSSIBLE")
+        }
+
+        console.log(connections);
+    },
+    stop: function() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
+
+function Vector2D(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+
+
+function animate(){
+    window.requestAnimationFrame(animate);
+
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    
+
+    for (let l = 0; l < lines.length; l++) {
+        lines[l].draw();
+    }
+
+    for (let p = 0; p < points.length; p++) {
+        points[p].draw();
+
+        game.ctx.font = "16px Arial";
+        game.ctx.fillText(`${connections[p]}`, points[p].pos.x+5, points[p].pos.y+15)
+    }
+}
+
+game.start();
+animate()
